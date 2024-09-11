f6d8934894589b7b3864d892e86c3886649518f1
mbhagat13 <meherbhagat97@gmail.com>
2024-09-10 21:24:38 -0400

    Adding remaining local code

--- config/routes.rb
+++ config/routes.rb
@@ -26,9 +26,9 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
-
   delete "logout", to: "sessions#destroy"
 
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
   #GET root
   root to: "main#index"
